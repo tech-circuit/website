5261945a7bf801115fd93bfad325a673a9dc0387
Anshul Saha <anshulsaha15@gmail.com>
2021-05-06 19:32:02 +0530

    navbar init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Login from './components/Login';
+import Navbar from './components/Navbar';
+import Index from './components/Index'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>TechCircuit</h1>
-      <Login />
-    </div>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/"><div className="container"><Index/></div></Route>
+        <Route exact path="/login"><div className="container"><Login/></div></Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Navbar extends Component {
+    render(){
+        return (
+            <>
+            <nav>
+                <div className="nav-wrapper">
+                    <Link to="/"><span><img src="/assets/fulllogo.png" alt="tC logo" className="logo"/></span></Link>
+                    <div className="nav-links">
+                      <div className="nav-link"><Link to="/work" class="link">Work</Link></div>
+                      <div className="nav-link"><Link to="/community" class="link">Community</Link></div>
+                      <div className="nav-link"><Link to="/about" class="link">About</Link></div>
+                      <div className="nav-link"><Link to="/forum" class="link">Forum</Link></div>
+                      <div className="nav-link"><Link to="/resources" class="link">Resources</Link></div>
+                      <div className="nav-link"><Link to="/contact" class="link">Contact</Link></div>
+                      <div className="nav-link"><Link to="/server" class="link"><button className="discord-link-button" id="discord-button">Discord</button></Link></div>
+                      <div className="nav-right">
+                        <div className="nav-link"><Link to="/profile"><span><img src="/assets/accounticon.png" alt="tC logo" className="account-logo"/></span></Link></div>
+                        {/* <div className="nav-link"><Link to="/register" class="link"><button className="register-link-button" id="discord-button">Sign up</button></Link></div>
+                        <div className="nav-link"><Link to="/login" class="link"><button className="login-link-button" id="discord-button">Log in</button></Link></div> */}
+                      </div>
+                    </div>
+                </div>
+            </nav>
+          </>
+        )
+    }
+}
+
+export default Navbar;
