f148a4502bb4b3287d34f0a8f43003f178c344f1
Anshul Saha <anshulsaha15@gmail.com>
2021-05-12 12:16:04 +0530

    nav updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,15 +10,16 @@ class Navbar extends Component {
                     <Link to="/"><span><img src="/assets/fulllogo.png" alt="tC logo" className="logo"/></span></Link>
                     <div className="nav-links">
                       <div className="nav-link"><Link to="/work" class="link">Work</Link></div>
-                      <div className="nav-link"><Link to="/community" class="link">Community</Link></div>
-                      <div className="nav-link"><Link to="/about" class="link">About</Link></div>
-                      <div className="nav-link"><Link to="/forum" class="link">Forum</Link></div>
+                      <div className="nav-link"><Link to="/community" class="link">Events</Link></div>
+                      <div className="nav-link"><Link to="/about" class="link">Forums</Link></div>
+                      <div className="nav-link"><Link to="/forum" class="link">Community</Link></div>
                       <div className="nav-link"><Link to="/resources" class="link">Resources</Link></div>
-                      <div className="nav-link"><Link to="/contact" class="link">Contact</Link></div>
+                      <div className="nav-link"><Link to="/contact" class="link">About</Link></div>
                       <div className="nav-link"><Link to="/server" class="link"><button className="discord-link-button" id="discord-button">Discord</button></Link></div>
                       <div className="nav-right">
-                        {/* <div className="nav-link"><Link to="/profile"><span><img src="/assets/accounticon.png" alt="tC logo" className="account-logo"/></span></Link></div> */}
-                        <div className="nav-link"><Link to="/login" class="link"><button className="login-link-button" id="discord-button">Log in</button></Link></div>
+                        {/* <div className="nav-link"><Link to="/profile"><span><img src="/assets/notif.png" alt="tC logo" className="notif-logo"/></span></Link></div>
+                        <div className="nav-link"><Link to="/profile"><span><img src="/assets/account.png" alt="tC logo" className="account-logo"/></span></Link></div> */}
+                        <div className="nav-link"><Link to="/login" class="link"><button className="login-link-button" id="discord-button">Login</button></Link></div>
                       </div>
                     </div>
                 </div>
