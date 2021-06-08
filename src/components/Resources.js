a824664c214b52c1ac643a78ba44a221e034f6ad
Vedanta Somnathe <vedusom142005@gmail.com>
2021-06-08 13:42:12 +0530

    resources rewrite

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -1,81 +1,86 @@
 import React from "react";
 import "../resources.css";
+import { FaSearch } from "react-icons/fa";
 
 const Resources = () => {
-    return (
-      <div className="main-wrap">
-        <div className="intro-div">
-          <h1 className="heading">Resources, Prompts & More</h1>
-          <h3 className="text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Condimentum
-            consectetur ac cursus tortor. Et feugiat in ipsum morbi molestie amet
-            massa.
-          </h3>
-          <div className="resources-search" style={{ marginLeft: "-10px" }}>
-            <img
-              className="searchicon"
-              src="/assets/magnifying-glass.svg"
-              alt="magnifying-glass"
-            />
-            <input
-              className="searchbar"
-              type="text"
-              placeholder="Search fields, docs, archives"
-            />
-          </div>
-        </div>
-        <div className="resources-div">
-          <div className="subheading-div" width="100%">
-            <h1 className="subheading">Resources</h1>
-            <div className="resources">
-              <div className="resource">
-                <h2 className="resource-text">Design & Visuals</h2>
-              </div>
-              <div className="resource">
-                <h2 className="resource-text">Design & Visuals</h2>
-              </div>
-              <div className="resource">
-                <h2 className="resource-text">Design & Visuals</h2>
-              </div>
-              <div className="resource">
-                <h2 className="resource-text">Design & Visuals</h2>
-              </div>
-              <div className="resource">
-                <h2 className="resource-text">Design & Visuals</h2>
-              </div>
-              <div className="resource">
-                <h2 className="resource-text">Design & Visuals</h2>
-              </div>
-              <div className="resource">
-                <h2 className="resource-text">Design & Visuals</h2>
-              </div>
-              <div className="resource">
-                <h2 className="resource-text">Design & Visuals</h2>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="resources-div" style={{ marginBottom: "191px" }}>
-          <div className="subheading-div" width="100%">
-            <h1 className="subheading">Prompts, Papers & More</h1>
-            <div className="resources">
-              <div className="resource">
-                <h2 className="resource-text">Design & Visuals</h2>
-              </div>
-              <div className="resource">
-                <h2 className="resource-text">Design & Visuals</h2>
-              </div>
-              <div className="resource">
-                <h2 className="resource-text">Design & Visuals</h2>
-              </div>
-              <div className="resource">
-                <h2 className="resource-text">Design & Visuals</h2>
-              </div>
-            </div>
-          </div>
+  return (
+    <div className="container rsc-cont">
+      <div className="rsc-head">
+        <h1>Resources, Prompts & More</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Condimentum
+          consectetur ac cursus tortor. Et feugiat in ipsum morbi molestie amet
+          massa.
+        </p>
+        <div className="input">
+          <FaSearch />
+          <input type="text" placeholder="Search fields, docs, archives" />
         </div>
       </div>
-    );
-}
+
+      <div className="resources">
+        <h2>Resources</h2>
+        <div className="rsc-cards">
+          <a href="/" className="resource">
+            Design and Visuals
+          </a>
+          <a href="/" className="resource">
+            Design and Visuals
+          </a>
+          <a href="/" className="resource">
+            Design and Visuals
+          </a>
+          <a href="/" className="resource">
+            Design and Visuals
+          </a>
+
+          <a href="/" className="resource">
+            Design and Visuals
+          </a>
+          <a href="/" className="resource">
+            Design and Visuals
+          </a>
+          <a href="/" className="resource">
+            Design and Visuals
+          </a>
+          <a href="/" className="resource">
+            Design and Visuals
+          </a>
+        </div>
+      </div>
+
+      <div className="resources">
+        <h2>Resources</h2>
+        <div className="rsc-cards">
+          <a href="/" className="resource">
+            Design and Visuals
+          </a>
+          <a href="/" className="resource">
+            Design and Visuals
+          </a>
+          <a href="/" className="resource">
+            Design and Visuals
+          </a>
+          <a href="/" className="resource">
+            Design and Visuals
+          </a>
+
+          <a href="/" className="resource">
+            Design and Visuals
+          </a>
+          <a href="/" className="resource">
+            Design and Visuals
+          </a>
+          <a href="/" className="resource">
+            Design and Visuals
+          </a>
+          <a href="/" className="resource">
+            Design and Visuals
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Resources;
