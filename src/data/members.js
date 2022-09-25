467bdaa114ede3d0b11aef8907f57577e3947d5e
Vedanta Somnathe <vedanta1412@gmail.com>
2022-09-25 13:12:25 +0530

    arre yaar

--- src/data/members.js
+++ src/data/members.js
@@ -1,44 +1,44 @@
 const members = [
-  {
-    image: "/assets/samvr.jpeg",
-    name: "Samvrant Samantaray",
-    position: "Design Head"
-  },
-  {
-    image: "https://github.com/sheldor1510.png",
-    name: "Anshul Saha",
-    position: "Development Head"
-  },
-  {
-    image: "/assets/thevedan.jpeg",
-    name: "Vedanta Somnathe",
-    position: "Developer"
-  },
-  {
-    image: "https://github.com/ribhavsharma.png",
-    name: "Ribhav Sharma",
-    position: "Logistics"
-  },
-  {
-    image: "/assets/mahika.jpg",
-    name: "Mahika Sehrawat",
-    position: "Developer"
-  },
-  {
-    image: "/assets/kavin.jpeg",
-    name: "Kavin Valli",
-    position: "Developer"
-  },
-  {
-    image: "/assets/inesh.jpeg",
-    name: "Inesh Tickoo",
-    position: "Outreach and Design"
-  },
-  {
-    image: "/assets/abhishree.jpg",
-    name: "Abhishree Bhardwaj",
-    position: "Designer"
-  },
-]
+    {
+        image: "/assets/samvr.jpeg",
+        name: "Samvrant Samantaray",
+        position: "Design Head",
+    },
+    {
+        image: "https://github.com/sheldor1510.png",
+        name: "Anshul Saha",
+        position: "Development Head",
+    },
+    {
+        image: "/assets/thevedan.jpeg",
+        name: "Vedanta Somnathe",
+        position: "Developer",
+    },
+    {
+        image: "https://github.com/ribhavsharma.png",
+        name: "Ribhav Sharma",
+        position: "Logistics",
+    },
+    {
+        image: "/assets/mahika.jpg",
+        name: "Mahika Sehrawat",
+        position: "Developer",
+    },
+    {
+        image: "/assets/kavin.jpeg",
+        name: "Kavin Valli",
+        position: "Developer",
+    },
+    {
+        image: "/assets/inesh.jpeg",
+        name: "Inesh Tickoo",
+        position: "Outreach and Design",
+    },
+    {
+        image: "/assets/abhishree.jpg",
+        name: "Abhishree Bhardwaj",
+        position: "Designer",
+    },
+];
 
 export default members;
