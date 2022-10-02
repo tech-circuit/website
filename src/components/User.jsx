802e7a3af131ebcc4d3b6575f46ca81b81b846eb
Vedanta Somnathe <vedanta1412@gmail.com>
2022-10-03 01:34:10 +0530

    user profile in a box + other minor UI fixes

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -59,89 +59,95 @@ const User = () => {
                 onClick={close}
             ></div>
             <section className="user-cont">
-                <Link className="user-back" to="/community">
-                    <FaChevronLeft />
-                    Back
-                </Link>
-                <div className="profile-info">
-                    <img src={user.pfp_url} className="user-pfp" alt="alt" />
-                    <div className="text">
-                        <div className="e">
-                            <h3>{user.name}</h3>
-                            <p className="field">{user.title}</p>
+                <div className="user-cont-box">
+                    <Link className="user-back" to="/community">
+                        <FaChevronLeft />
+                        Back
+                    </Link>
+                    <div className="profile-info">
+                        <img
+                            src={user.pfp_url}
+                            className="user-pfp"
+                            alt="alt"
+                        />
+                        <div className="text">
+                            <div className="e">
+                                <h3>{user.name}</h3>
+                                <p className="field">{user.title}</p>
+                            </div>
+                            <p className="email">{user.email}</p>
+                            <div className="user-links">
+                                {user.links
+                                    ? user.links
+                                          .slice(0)
+                                          .reverse()
+                                          .map((link) => {
+                                              return (
+                                                  <a
+                                                      href={link}
+                                                      target="_blank"
+                                                      rel="noreferrer"
+                                                  >
+                                                      {getLinkogo(link)}
+                                                  </a>
+                                              );
+                                          })
+                                    : ""}
+                            </div>
                         </div>
-                        <p className="email">{user.email}</p>
-                        <div className="user-links">
-                            {user.links
-                                ? user.links
-                                      .slice(0)
-                                      .reverse()
-                                      .map((link) => {
-                                          return (
-                                              <a
-                                                  href={link}
-                                                  target="_blank"
-                                                  rel="noreferrer"
-                                              >
-                                                  {getLinkogo(link)}
-                                              </a>
-                                          );
-                                      })
-                                : ""}
+                    </div>
+                    {user.about && (
+                        <div className="user-div aboutme">
+                            <h3>About Me</h3>
+                            <p>{user.about}</p>
                         </div>
-                    </div>
-                </div>
-                {user.about && (
-                    <div className="user-div aboutme">
-                        <h3>About Me</h3>
-                        <p>{user.about}</p>
-                    </div>
-                )}
-                {user.skills && user.skills.length !== 0 && (
-                    <div className="user-div skills">
-                        <h3>Skills</h3>
-                        <p>
-                            {user.skills.map((skill, i) => {
-                                return <span key={i}>{skill} </span>;
-                            })}
-                        </p>
-                    </div>
-                )}
-                {orgs && orgs.length !== 0 && (
-                    <div className="user-div current-org">
-                        <h3>Organisations</h3>
-                        {orgs.map((org) => (
-                            <MiniOrgCard
-                                key={org._id}
-                                org={org}
-                                leave={false}
-                            />
-                        ))}
-                    </div>
-                )}
-                {projects && projects.length !== 0 && (
-                    <div className="user-div user-projects">
-                        <h3>Projects Showcase</h3>
-                        <div className="workCards">
-                            {projects.map((project) => (
-                                <ProjectCard
-                                    id={userId}
-                                    key={project._id}
-                                    view={view}
-                                    project={project}
+                    )}
+                    {user.skills && user.skills.length !== 0 && (
+                        <div className="user-div skills">
+                            <h3>Skills</h3>
+                            <p>
+                                {user.skills.map((skill, i) => {
+                                    return <span key={i}>{skill} </span>;
+                                })}
+                            </p>
+                        </div>
+                    )}
+                    {orgs && orgs.length !== 0 && (
+                        <div className="user-div current-org">
+                            <h3>Organisations</h3>
+                            {orgs.map((org) => (
+                                <MiniOrgCard
+                                    key={org._id}
+                                    org={org}
+                                    leave={false}
                                 />
                             ))}
                         </div>
-                    </div>
-                )}
+                    )}
+                    {projects && projects.length !== 0 && (
+                        <div className="user-div user-projects">
+                            <h3>Projects Showcase</h3>
+                            <div className="workCards">
+                                {projects.map((project) => (
+                                    <ProjectCard
+                                        id={userId}
+                                        key={project._id}
+                                        view={view}
+                                        project={project}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                    )}
 
-                <section
-                    className={
-                        fullView ? "fullEvent fullEventActive" : "fullEvent"
-                    }
-                >
-                    <FullProject project={selectedProject} close={close} />
-                </section>
+                    <section
+                        className={
+                            fullView ? "fullEvent fullEventActive" : "fullEvent"
+                        }
+                    >
+                        <FullProject project={selectedProject} close={close} />
+                    </section>
+                </div>
             </section>
         </>
     );
